e9c32fcc1f6ed9ed33df85d3e347ff93a2cb5e90
Seoungho Lee <ssho1204@naver.com>
2023-05-16 22:52:28 +0900

    drawerlayout

--- app/src/main/java/com/example/medi_keeper/MainActivity.java
+++ app/src/main/java/com/example/medi_keeper/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.medi_keeper;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setTitle("Medi-keeper");
+        actionBar.setHomeButtonEnabled(true);
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case android.R.id.home: // 툴바의 햄버거를 클릭하면 drawer 열리게
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
