5e48e3b9393a8a8c91f3c4d06c1d33d97e720360
Seoungho Lee <ssho1204@naver.com>
2023-05-17 00:37:31 +0900

    drawerlayout

--- app/src/main/java/com/example/medi_keeper/MainActivity.java
+++ app/src/main/java/com/example/medi_keeper/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.medi_keeper;
 
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
@@ -29,10 +32,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         switch (id){
             case android.R.id.home: // 툴바의 햄버거를 클릭하면 drawer 열리게
-
+                drawerLayout.openDrawer(Gravity.RIGHT);
                 break;
         }
 
