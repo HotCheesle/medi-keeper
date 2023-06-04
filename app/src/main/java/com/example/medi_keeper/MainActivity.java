79f571e2683122ad12c29af3edf5f6a2ec46792e
Seoungho Lee <ssho1204@naver.com>
2023-06-05 00:48:38 +0900

    네이버 API연결해보기

--- app/src/main/java/com/example/medi_keeper/MainActivity.java
+++ app/src/main/java/com/example/medi_keeper/MainActivity.java
@@ -3,44 +3,46 @@ package com.example.medi_keeper;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    private ActionBar actionBar;
+    private
+    android.support.v4.widget.DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Toolbar toolbar = findViewById(R.id.toolbar2);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        actionBar.setTitle("Medi-keeper");
-        actionBar.setHomeButtonEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
 
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    //@Override
+    public boolean onOptionItemSelected(MenuItem item){
         int id = item.getItemId();
-        drawerLayout = findViewById(R.id.drawer_layout);
-
         switch (id){
-            case android.R.id.home: // 툴바의 햄버거를 클릭하면 drawer 열리게
-                drawerLayout.openDrawer(Gravity.RIGHT);
+            case android.R.id.home:
+                drawerLayout.openDrawer(R.id.drawer_layout);
+
                 break;
         }
-
-        return super.onOptionsItemSelected(item);
+    return super.onOptionsItemSelected(item);
     }
 
 
