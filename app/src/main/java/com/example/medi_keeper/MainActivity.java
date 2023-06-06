bebcfe06ac2b739dfa0355dc608d87d8eeffd9ed
Seoungho Lee <ssho1204@naver.com>
2023-06-06 10:35:40 +0900

    네이버 API연결해보기

--- app/src/main/java/com/example/medi_keeper/MainActivity.java
+++ app/src/main/java/com/example/medi_keeper/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.medi_keeper;
 
-import android.support.annotation.NonNull;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
 
@@ -22,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText search_mname = findViewById(R.id.search_mname);
+        Button search_btn_frommain = findViewById(R.id.search_btn_frommain);
+
         drawerLayout = findViewById(R.id.drawer_layout);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -29,7 +30,19 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
+        search_btn_frommain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (search_mname.getText() != null) {
+                    Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                    String searchtext = search_mname.getText().toString();
+                    intent.putExtra("searchtext", searchtext);
+                    startActivities(new Intent[]{intent});
+                }
 
+                //SearchMedicine(String.valueOf(search_mname.getText()))
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/medi_keeper/SearchActivity.java
+++ app/src/main/java/com/example/medi_keeper/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.example.medi_keeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        EditText search_mname = findViewById(R.id.search_mname);
+
+        Intent intent = getIntent();
+        String searchtext = intent.getStringExtra("searchtext");
+        search_mname.setText(searchtext);
+
     }
 }
